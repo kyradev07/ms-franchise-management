0061ad195714491645eb07d5ade41888647a4db0
Kyra <kyra.dev.07@gmail.com>
2025-09-05 12:11:24 -0500

    se crea excepcion para el manejo de sucursales duplicadas

---
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/exceptions/DuplicateBranchException.java
@@ -0,0 +1,7 @@
+package co.com.bancolombia.usecase.exceptions;
+
+public class DuplicateBranchException extends RuntimeException {
+    public DuplicateBranchException(String message, String franchiseName) {
+        super("Branch with name: " + message + " already exists in Franchise" + franchiseName);
+    }
+}
