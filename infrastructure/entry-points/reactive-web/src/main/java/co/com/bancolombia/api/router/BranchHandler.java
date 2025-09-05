1eabd317ce490467a5cabaf77579a60ac62da4d4
Kyra <kyra.dev.07@gmail.com>
2025-09-05 14:00:23 -0500

    se agrega método en el manejador de sucursales para el caso de uso actualización de nombre de sucursal

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/router/BranchHandler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/router/BranchHandler.java
@@ -5,6 +5,7 @@ import co.com.bancolombia.api.mappers.BranchMapperDTO;
 import co.com.bancolombia.api.validations.FieldsValidator;
 import co.com.bancolombia.api.validations.MissingRequestBodyException;
 import co.com.bancolombia.usecase.in.branch.AddBranchToFranchiseUseCase;
+import co.com.bancolombia.usecase.in.branch.UpdateBranchNameUseCase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -17,10 +18,15 @@ import reactor.core.publisher.Mono;
 public class BranchHandler {
 
     private final AddBranchToFranchiseUseCase addBranchToFranchiseUseCase;
+    private final UpdateBranchNameUseCase updateBranchNameUseCase;
     private final FieldsValidator fieldsValidator;
 
-    public BranchHandler(AddBranchToFranchiseUseCase addBranchToFranchiseUseCase, FieldsValidator fieldsValidator) {
+    public BranchHandler(
+            AddBranchToFranchiseUseCase addBranchToFranchiseUseCase,
+            UpdateBranchNameUseCase updateBranchNameUseCase,
+            FieldsValidator fieldsValidator) {
         this.addBranchToFranchiseUseCase = addBranchToFranchiseUseCase;
+        this.updateBranchNameUseCase = updateBranchNameUseCase;
         this.fieldsValidator = fieldsValidator;
     }
 
@@ -33,5 +39,20 @@ public class BranchHandler {
                 .flatMap(branch -> this.addBranchToFranchiseUseCase.addBranchToFranchise(franchiseId, branch))
                 .map(BranchMapperDTO::toDTO)
                 .flatMap(ServerResponse.status(HttpStatus.CREATED)::bodyValue);
+    }
+
+    public Mono<ServerResponse> updateBranchName(ServerRequest serverRequest) {
+        String franchiseId = serverRequest.pathVariable("franchiseId");
+        String branchId = serverRequest.pathVariable("branchId");
+
+        return serverRequest.bodyToMono(BranchDTO.class)
+                .switchIfEmpty(Mono.error(new MissingRequestBodyException("Body cannot be null")))
+                .map(fieldsValidator::validate)
+                .map(BranchMapperDTO::toDomain)
+                .doOnNext(br -> br.setId(branchId))
+                .flatMap(branch -> this.updateBranchNameUseCase.updateName(franchiseId, branch))
+                .map(BranchMapperDTO::toDTO)
+                .flatMap(ServerResponse.status(HttpStatus.CREATED)::bodyValue);
+
     }
 }
