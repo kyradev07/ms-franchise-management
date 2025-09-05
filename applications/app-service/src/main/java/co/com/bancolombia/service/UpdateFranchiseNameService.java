a5d3b9a9afed479ed5e26546b92aadf62e7d6577
Kyra <kyra.dev.07@gmail.com>
2025-09-05 03:39:09 -0500

    se implementa caso de uso actualizar nombre de franquicia

--- applications/app-service/src/main/java/co/com/bancolombia/service/UpdateFranchiseNameService.java
+++ applications/app-service/src/main/java/co/com/bancolombia/service/UpdateFranchiseNameService.java
@@ -1,4 +1,38 @@
 package co.com.bancolombia.service;
 
-public class UpdateFranchiseNameService {
+import co.com.bancolombia.model.Franchise;
+import co.com.bancolombia.model.gateway.FranchiseRepositoryPort;
+import co.com.bancolombia.usecase.exceptions.DuplicateFranchiseException;
+import co.com.bancolombia.usecase.in.franchise.UpdateFranchiseNameUseCase;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Service
+public class UpdateFranchiseNameService implements UpdateFranchiseNameUseCase {
+
+    private final FranchiseRepositoryPort franchiseRepositoryPort;
+
+    public UpdateFranchiseNameService(FranchiseRepositoryPort franchiseRepositoryPort) {
+        this.franchiseRepositoryPort = franchiseRepositoryPort;
+    }
+
+    @Override
+    public Mono<Franchise> updateName(String franchiseId, String name) {
+        log.info("Updating Franchise Name {}", franchiseId);
+        String fixedName = name.trim().toLowerCase();
+
+        return Mono.defer(() -> this.franchiseRepositoryPort.findByName(fixedName)
+                .flatMap(franchiseDB -> {
+                    log.warn("Franchise with name {} already exists!", name);
+                    return Mono.<Franchise>error(new DuplicateFranchiseException(fixedName));
+                })
+                .switchIfEmpty(
+                        this.franchiseRepositoryPort.update(franchiseId, name)
+                                .doOnSuccess(updatedF -> log.info("Franchise {} created successfully!", updatedF.getName()))
+                                .doOnError(error -> log.error("Error while updating Franchise {}", error.getMessage()))
+                )
+        );
+    }
 }
