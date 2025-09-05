0bf6dcf4a6735abb6ddd0996b00c2d8ef77e1074
Kyra <kyra.dev.07@gmail.com>
2025-09-05 02:20:52 -0500

    se crea mapper para transformar de objeto de dominio a entidad de bd y viceversa

--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/mappers/FranchiseMapper.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/mappers/FranchiseMapper.java
@@ -1,4 +1,25 @@
 package co.com.bancolombia.mongo.mappers;
 
+import co.com.bancolombia.model.Branch;
+import co.com.bancolombia.model.Franchise;
+import co.com.bancolombia.mongo.documents.BranchDocument;
+import co.com.bancolombia.mongo.documents.FranchiseDocument;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class FranchiseMapper {
+
+    public static FranchiseDocument toDocument(Franchise franchise) {
+        List<BranchDocument> branches = new ArrayList<>();
+        return new FranchiseDocument(franchise.getId(), franchise.getName(), branches);
+    }
+
+    public static Franchise toDomain(FranchiseDocument franchise) {
+        log.debug("Converting Franchise Document to Franchise.");
+        List<Branch> branches = new ArrayList<>();
+        return new Franchise(franchise.getId(), franchise.getName(), branches);
+    }
 }
