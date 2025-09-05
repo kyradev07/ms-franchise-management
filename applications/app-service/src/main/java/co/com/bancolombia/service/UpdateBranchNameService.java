b39f858bd46e686f339b385eb574c6b5a44c65c6
Kyra <kyra.dev.07@gmail.com>
2025-09-05 13:51:07 -0500

    se modifican parámetros de la interfaz y se implementa el caso de uso para actualizar el nombre de una sucursal

---
+++ applications/app-service/src/main/java/co/com/bancolombia/service/UpdateBranchNameService.java
@@ -0,0 +1,61 @@
+package co.com.bancolombia.service;
+
+import co.com.bancolombia.model.Branch;
+import co.com.bancolombia.model.gateway.FranchiseRepositoryPort;
+import co.com.bancolombia.usecase.exceptions.BranchNotFoundException;
+import co.com.bancolombia.usecase.exceptions.DuplicateBranchException;
+import co.com.bancolombia.usecase.in.branch.UpdateBranchNameUseCase;
+import co.com.bancolombia.utils.Filters;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class UpdateBranchNameService implements UpdateBranchNameUseCase {
+
+    private final FranchiseRepositoryPort franchiseRepositoryPort;
+
+    public UpdateBranchNameService(FranchiseRepositoryPort franchiseRepositoryPort) {
+        this.franchiseRepositoryPort = franchiseRepositoryPort;
+    }
+
+    @Override
+    public Mono<Branch> updateName(String franchiseId, Branch branch) {
+        log.info("Updating Branch Name {}", branch.getName());
+
+        return Mono.defer(() -> this.franchiseRepositoryPort.findById(franchiseId)
+                .flatMap(franchise -> {
+
+                    Branch branchDb = franchise.getBranches()
+                            .stream()
+                            .filter(br -> br.getId().equals(branch.getId()))
+                            .findFirst().orElse(null);
+
+                    if (branchDb == null) {
+                        log.warn("Branch with id {} does not exists in Franchise {}", branch.getId(), franchise.getName());
+                        return Mono.error(new BranchNotFoundException(branch.getId()));
+                    }
+
+                    boolean existName = Filters.filterByName(franchise, branch.getName());
+
+                    if (existName) {
+                        log.warn("Branch with name {} already exists in Franchise", branch.getName());
+                        return Mono.error(new DuplicateBranchException(branch.getName(), franchise.getName()));
+                    }
+
+                    branchDb.setName(branch.getName());
+
+                    return this.franchiseRepositoryPort.save(franchise)
+                            .map(fr -> franchise.getBranches()
+                                    .stream()
+                                    .filter(br -> br.getId().equals(branch.getId()))
+                                    .findFirst().orElse(branch)
+                            )
+                            .doOnSuccess(updatedF -> log.info("Branch name {} was updated successfully!", updatedF.getName()))
+                            .doOnError(error -> log.error("Error while updating Branch name {}", error.getMessage()));
+                })
+
+        );
+    }
+}
--- domain/usecase/src/main/java/co/com/bancolombia/usecase/in/branch/UpdateBranchNameUseCase.java
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/in/branch/UpdateBranchNameUseCase.java
@@ -1,8 +1,8 @@
-package co.com.bancolombia.usecase.in;
+package co.com.bancolombia.usecase.in.branch;
 
 import co.com.bancolombia.model.Branch;
 import reactor.core.publisher.Mono;
 
 public interface UpdateBranchNameUseCase {
-    Mono<Branch> updateName(String franchiseId, String branchId, String name);
+    Mono<Branch> updateName(String franchiseId, Branch branch);
 }
