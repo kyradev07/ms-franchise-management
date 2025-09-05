db2a0dae409ec17ca02c32c20039238fdbfc1eb1
Kyra <kyra.dev.07@gmail.com>
2025-09-05 09:03:16 -0500

    se agrega cambia de directorio el caso de uso

--- domain/usecase/src/main/java/co/com/bancolombia/usecase/in/UpdateBranchNameUseCase.java
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/in/UpdateBranchNameUseCase.java
@@ -1,4 +1,8 @@
 package co.com.bancolombia.usecase.in;
 
+import co.com.bancolombia.model.Branch;
+import reactor.core.publisher.Mono;
+
 public interface UpdateBranchNameUseCase {
+    Mono<Branch> updateName(String franchiseId, String branchId, String name);
 }
