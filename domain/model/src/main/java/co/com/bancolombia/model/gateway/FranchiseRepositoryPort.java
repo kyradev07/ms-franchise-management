8b82a3194c5504a2c530e9fcea01c6d43b16b228
Kyra <kyra.dev.07@gmail.com>
2025-09-05 02:13:57 -0500

    se define interfaz de persistencia para el modelo franquicia

---
+++ domain/model/src/main/java/co/com/bancolombia/model/gateway/FranchiseRepositoryPort.java
@@ -0,0 +1,12 @@
+package co.com.bancolombia.model.gateway;
+
+import co.com.bancolombia.model.Franchise;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface FranchiseRepositoryPort {
+    Mono<Franchise> save(Franchise franchise);
+    Mono<Franchise> findById(String id);
+    Mono<Franchise> findByName(String name);
+    Flux<Franchise> findAll();
+}
