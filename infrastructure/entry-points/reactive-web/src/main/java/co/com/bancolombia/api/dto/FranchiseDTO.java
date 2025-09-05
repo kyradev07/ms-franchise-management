2908465fff8bfb37a4b208c128f689af9de3fa63
Kyra <kyra.dev.07@gmail.com>
2025-09-05 16:21:05 -0500

    se agregan anotaciones de lombok para el dto de franquicias

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/FranchiseDTO.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/FranchiseDTO.java
@@ -2,28 +2,17 @@ package co.com.bancolombia.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FranchiseDTO {
 
     @NotNull(message = "Franchise name is required")
     @NotBlank(message = "Franchise must not be blank")
     private String name;
-
-
-    public FranchiseDTO() {
-
-    }
-
-    public FranchiseDTO(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
