2845653cbe3ed25fd63f9f1ef752334a92deb1ba
Kyra <kyra.dev.07@gmail.com>
2025-09-05 16:08:56 -0500

    se ajusta modelo de dominio de producto

--- domain/model/src/main/java/co/com/bancolombia/model/Product.java
+++ domain/model/src/main/java/co/com/bancolombia/model/Product.java
@@ -1,5 +1,37 @@
 package co.com.bancolombia.model;
 
-public record Product(String id, String name, Integer stock) {
+public class Product {
+    private String id;
+    private String name;
+    private Integer stock;
 
+    public Product(String id, String name, Integer stock) {
+        this.id = id;
+        this.name = name;
+        this.stock = stock;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
 }
