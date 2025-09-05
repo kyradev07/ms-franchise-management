b1fe210daba1cbc8943b83d317374baa2cac17a2
Kyra <kyra.dev.07@gmail.com>
2025-09-05 02:25:48 -0500

    se configura routerfunctions para recibir los diferentes handlers

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/RouterRest.java
@@ -1,20 +1,33 @@
 package co.com.bancolombia.api;
 
+import co.com.bancolombia.api.router.FranchiseHandler;
+import co.com.bancolombia.api.validations.ValidationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class RouterRest {
+
     @Bean
+    public RouterFunction<ServerResponse> routerFunction(FranchiseHandler franchiseHandler, ValidationFilter validationFilter) {
+        return route()
+                .POST("api/v1/franchise", franchiseHandler::createFranchise)
+                .filter(validationFilter)
+                .build();
+    }
+
+
+    /*@Bean
     public RouterFunction<ServerResponse> routerFunction(Handler handler) {
         return route(GET("/api/usecase/path"), handler::listenGETUseCase)
                 .andRoute(POST("/api/usecase/otherpath"), handler::listenPOSTUseCase)
                 .and(route(GET("/api/otherusercase/path"), handler::listenGETOtherUseCase));
-    }
+    }*/
 }
