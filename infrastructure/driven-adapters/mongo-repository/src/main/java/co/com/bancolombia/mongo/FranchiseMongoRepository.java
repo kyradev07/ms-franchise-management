7d7a97c665025526fe786adfd06f8abe139dc4ba
Kyra <kyra.dev.07@gmail.com>
2025-09-05 02:17:08 -0500

    se crea interfaz de persistencia con las operaciones crud de mongo reactivo

--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/FranchiseMongoRepository.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/FranchiseMongoRepository.java
@@ -1,4 +1,12 @@
 package co.com.bancolombia.mongo;
 
-public interface FranchiseMongoRepository {
+import co.com.bancolombia.mongo.documents.FranchiseDocument;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface FranchiseMongoRepository extends ReactiveMongoRepository<FranchiseDocument, String> {
+
+    Mono<FranchiseDocument> findByName(String name);
 }
