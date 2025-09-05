3f4ab376be1e7ca1d688ea037489317d37abd637
Kyra <kyra.dev.07@gmail.com>
2025-09-05 16:06:22 -0500

    Se implementan métodos de búsqueda para la consulta de sucursales y productos

--- applications/app-service/src/main/java/co/com/bancolombia/utils/Filters.java
+++ applications/app-service/src/main/java/co/com/bancolombia/utils/Filters.java
@@ -2,18 +2,41 @@ package co.com.bancolombia.utils;
 
 import co.com.bancolombia.model.Branch;
 import co.com.bancolombia.model.Franchise;
+import co.com.bancolombia.model.Product;
 
 public class Filters {
-    public static boolean filterByName(Franchise franchise, String name) {
+    public static boolean filterBranchByName(Franchise franchise, String name) {
         return franchise.getBranches()
                 .stream()
                 .anyMatch(fBranch -> fBranch.getName().equals(name));
+    }
+
+    public static boolean filterProductByName(Branch branch, String name) {
+        return branch.getProducts()
+                .stream()
+                .anyMatch(product -> product.getName().equals(name));
+    }
+
+    public static Branch filterBranchById(Franchise franchise, String branchId) {
+        return franchise.getBranches()
+                .stream()
+                .filter(br -> br.getId().equals(branchId))
+                .findFirst().orElse(null);
     }
 
     public static Branch findBranch(Franchise franchise, Branch branch) {
         return franchise.getBranches()
                 .stream()
                 .filter(br -> br.getId().equals(branch.getId()))
-                .findFirst().orElse(branch);
+                .findFirst().orElse(null);
+    }
+
+    public static Product findProduct(Franchise franchise, String brId, String prId) {
+        return franchise.getBranches()
+                .stream()
+                .filter(br -> br.getId().equals(brId))
+                .flatMap(pr -> pr.getProducts().stream())
+                .filter(pr -> pr.getId().equals(prId))
+                .findFirst().orElse(null);
     }
 }
