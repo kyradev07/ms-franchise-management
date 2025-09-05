5bbe1c7bdc2e5f4241ccd24aa5bf37223d236a2a
Kyra <kyra.dev.07@gmail.com>
2025-09-05 03:48:12 -0500

    se agrega ruta para actualizar nombre de franquicia

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/RouterRest.java
@@ -5,11 +5,9 @@ import co.com.bancolombia.api.validations.ValidationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.*;
-import static org.springframework.web.reactive.function.server.RouterFunctions.*;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -18,16 +16,10 @@ public class RouterRest {
     @Bean
     public RouterFunction<ServerResponse> routerFunction(FranchiseHandler franchiseHandler, ValidationFilter validationFilter) {
         return route()
-                .POST("api/v1/franchise", franchiseHandler::createFranchise)
-                .filter(validationFilter)
-                .build();
+                .nest(path("api/v1/franchise"), builder -> builder
+                        .POST("", franchiseHandler::createFranchise)
+                        .PUT("/{id}", franchiseHandler::updateFranchiseName)
+                ).build();
+
     }
-
-
-    /*@Bean
-    public RouterFunction<ServerResponse> routerFunction(Handler handler) {
-        return route(GET("/api/usecase/path"), handler::listenGETUseCase)
-                .andRoute(POST("/api/usecase/otherpath"), handler::listenPOSTUseCase)
-                .and(route(GET("/api/otherusercase/path"), handler::listenGETOtherUseCase));
-    }*/
 }
