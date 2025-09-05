88cbaa4e3f3a4b5dd9826a3506e2d8ee7c0bb70b
Kyra <kyra.dev.07@gmail.com>
2025-09-05 02:19:54 -0500

    se crean mappers para transformar de dto a objetos de dominio

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/mappers/BranchMapperDTO.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/mappers/BranchMapperDTO.java
@@ -1,4 +1,22 @@
 package co.com.bancolombia.api.mappers;
 
+import co.com.bancolombia.api.dto.BranchDTO;
+import co.com.bancolombia.model.Branch;
+import co.com.bancolombia.model.Product;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class BranchMapperDTO {
+
+    public static Branch toDomain(BranchDTO branchDTO) {
+        log.debug("Converting BranchDTO to BranchModel");
+
+        List<Product> productDTOS = new ArrayList<>();
+
+        return new Branch(null, branchDTO.getName(), productDTOS);
+
+    }
 }
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/mappers/FranchisMapperDTO.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/mappers/FranchisMapperDTO.java
@@ -1,4 +1,25 @@
 package co.com.bancolombia.api.mappers;
 
+import co.com.bancolombia.api.dto.FranchiseDTO;
+import co.com.bancolombia.model.Branch;
+import co.com.bancolombia.model.Franchise;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class FranchisMapperDTO {
+
+    public static Franchise toDomain(FranchiseDTO franchiseDTO) {
+        log.debug("Converting FranchiseDTO to FranchiseModel");
+
+        List<Branch> branches = new ArrayList<>();
+
+        return new Franchise(null, franchiseDTO.getName().trim().toLowerCase(), branches);
+    }
+
+    public static FranchiseDTO toDTO(Franchise franchise) {
+        return new FranchiseDTO(franchise.getName());
+    }
 }
