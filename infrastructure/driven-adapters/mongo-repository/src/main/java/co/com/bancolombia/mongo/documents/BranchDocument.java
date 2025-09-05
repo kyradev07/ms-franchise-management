de32513086c8d287e93f79fa3ef457e7e9cf1ed5
Kyra <kyra.dev.07@gmail.com>
2025-09-05 02:09:27 -0500

    se definen las entidades(colecciones) de persistencia para la BD

--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/documents/BranchDocument.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/documents/BranchDocument.java
@@ -1,4 +1,17 @@
 package co.com.bancolombia.mongo.documents;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BranchDocument {
+    private String id;
+    private String name;
+    private List<ProductDocument> products;
+
 }
--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/documents/ProductDocument.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/documents/ProductDocument.java
@@ -1,4 +1,16 @@
 package co.com.bancolombia.mongo.documents;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductDocument {
+
+    private String id;
+    private String name;
+    private Integer stock;
+
 }
