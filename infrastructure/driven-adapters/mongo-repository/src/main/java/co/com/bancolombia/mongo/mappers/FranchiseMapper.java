cee4de71fe67892f7a2d1f6bc91acf7f13caa123
Kyra <kyra.dev.07@gmail.com>
2025-09-05 12:12:37 -0500

    se ajusta mapper de franquicia para permitir agregar sucursales. Se utiliza implemetación mutable

--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/mappers/FranchiseMapper.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/mappers/FranchiseMapper.java
@@ -2,24 +2,45 @@ package co.com.bancolombia.mongo.mappers;
 
 import co.com.bancolombia.model.Branch;
 import co.com.bancolombia.model.Franchise;
+import co.com.bancolombia.model.Product;
 import co.com.bancolombia.mongo.documents.BranchDocument;
 import co.com.bancolombia.mongo.documents.FranchiseDocument;
+import co.com.bancolombia.mongo.documents.ProductDocument;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class FranchiseMapper {
 
     public static FranchiseDocument toDocument(Franchise franchise) {
-        List<BranchDocument> branches = new ArrayList<>();
+        List<BranchDocument> branches = franchise.getBranches() == null
+                ? new ArrayList<>()
+                : franchise.getBranches().stream().map(FranchiseMapper::toBranchDocument).toList();
+
         return new FranchiseDocument(franchise.getId(), franchise.getName(), branches);
+    }
+
+    private static BranchDocument toBranchDocument(Branch branch) {
+        List<ProductDocument> products = new ArrayList<>();
+        return new BranchDocument(branch.getId(), branch.getName(), products);
     }
 
     public static Franchise toDomain(FranchiseDocument franchise) {
         log.debug("Converting Franchise Document to Franchise.");
-        List<Branch> branches = new ArrayList<>();
+        List<Branch> branches = franchise.getBranches() == null
+                ? new ArrayList<>()
+                : franchise.getBranches().stream().map(FranchiseMapper::toBranchDomain)
+                        .collect(Collectors.toCollection(ArrayList::new));
+
         return new Franchise(franchise.getId(), franchise.getName(), branches);
     }
+
+    private static Branch toBranchDomain(BranchDocument branchDocument) {
+        List<Product> products = new ArrayList<>();
+        return new Branch(branchDocument.getId(), branchDocument.getName(), products);
+    }
+
 }
