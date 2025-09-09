73cc651f481a9c36a0ab743abc4a7a87c698e5b4
Kyra <kyra.dev.07@gmail.com>
2025-09-08 22:10:01 -0500

    se cambia formato del mensaje

--- domain/usecase/src/main/java/co/com/bancolombia/usecase/exceptions/DuplicateBranchException.java
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/exceptions/DuplicateBranchException.java
@@ -2,6 +2,6 @@ package co.com.bancolombia.usecase.exceptions;
 
 public class DuplicateBranchException extends RuntimeException {
     public DuplicateBranchException(String message, String franchiseName) {
-        super("Branch with name: " + message + " already exists in Franchise: " + franchiseName);
+        super("Branch with name <" + message + "> already exists in Franchise <" + franchiseName + ">");
     }
 }
--- domain/usecase/src/main/java/co/com/bancolombia/usecase/exceptions/DuplicateFranchiseException.java
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/exceptions/DuplicateFranchiseException.java
@@ -2,6 +2,6 @@ package co.com.bancolombia.usecase.exceptions;
 
 public class DuplicateFranchiseException extends RuntimeException {
     public DuplicateFranchiseException(String message) {
-        super("Franchise with name: " + message + " already exists");
+        super("Franchise with name <" + message + "> already exists");
     }
 }
