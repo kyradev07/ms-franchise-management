e76e2fbdd618f34693b4137ee1490b8a9d5762ec
Kyra <kyra.dev.07@gmail.com>
2025-09-08 19:51:47 -0500

    se agrega anotación, para manejar valor único para el campo nombre en la franquicia

--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/documents/FranchiseDocument.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/documents/FranchiseDocument.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -17,7 +18,7 @@ public class FranchiseDocument {
     @Id
     private String id;
 
-
+    @Indexed(unique=true)
     private String name;
 
     private List<BranchDocument> branches;
