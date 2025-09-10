eaa5d517f5694a141dabdd576ee36b7d9d358670
Kyra <kyra.dev.07@gmail.com>
2025-09-09 21:50:00 -0500

    se agrega condición y excepción para validar los datos en el dto de producto, para cuando se quiere eliminar un producto

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/mappers/ProductMapperDTO.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/mappers/ProductMapperDTO.java
@@ -1,6 +1,7 @@
 package co.com.bancolombia.api.mappers;
 
 import co.com.bancolombia.api.dto.ProductDTO;
+import co.com.bancolombia.api.validations.MissingRequestBodyException;
 import co.com.bancolombia.model.Product;
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,8 +10,11 @@ public class ProductMapperDTO {
     public static Product toDomain(ProductDTO productDTO) {
         log.debug("Converting ProductDTO to ProductModel");
 
-        Integer INITIAL_STOCK = 0;
-        return new Product(productDTO.getProductId(), productDTO.getName(), INITIAL_STOCK);
+        if ((productDTO.getName() == null || productDTO.getName().isBlank()) && (productDTO.getStock() == null || productDTO.getStock() < 0)) {
+            throw new MissingRequestBodyException("Product name and stock cannot be null, empty or negative number");
+        }
+
+        return new Product(productDTO.getProductId(), productDTO.getName(), productDTO.getStock());
     }
 
     public static ProductDTO toDTO(Product product) {
