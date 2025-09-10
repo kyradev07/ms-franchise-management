3f789b37df7b6f57e09b81eb3b1d7b12ba7fbeb4
Kyra <kyra.dev.07@gmail.com>
2025-09-09 19:10:50 -0500

    Se ajusta interfaz con los parámetros correspondientes para el caso de uso actualización producto

--- domain/usecase/src/main/java/co/com/bancolombia/usecase/in/product/UpdateProductUseCase.java
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/in/product/UpdateProductUseCase.java
@@ -3,6 +3,6 @@ package co.com.bancolombia.usecase.in.product;
 import co.com.bancolombia.model.Product;
 import reactor.core.publisher.Mono;
 
-public interface UpdateProductNameUseCase {
-    Mono<Product> updateName(String name);
+public interface UpdateProductUseCase {
+    Mono<Product> updateProduct(String franchiseId, String branchId, Product product);
 }
